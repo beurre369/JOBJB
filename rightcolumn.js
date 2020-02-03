5c7a06ccb5a4ab518ed2efe628f190a686a51f0a
beurre369 <58581514+beurre369@users.noreply.github.com>
2020-02-04 03:44:12 +0900

    Create rightcolumn.js

---
+++ rightcolumn.js
@@ -0,0 +1,6 @@
+var index = ['Contact'];
+        var i =0;
+        while(i < index.length){
+          document.write('<li><a href="'+index[i]+'.html">'+index[i]+'</a></li>');
+          i = i+1;
+          }

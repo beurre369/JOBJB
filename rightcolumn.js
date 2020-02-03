c0a7bc798e34c22372bebf83cd0a2d9a8f93a39f
beurre369 <58581514+beurre369@users.noreply.github.com>
2020-02-04 03:45:52 +0900

    Create rightcolumn.js

---
+++ rightcolumn.js
@@ -0,0 +1,7 @@
+ var index = ['별점', '관련 강의'];
+          var i =0;
+          while(i < index.length){
+            document.write('<li>'+index[i]+'</li>');
+            i = i+1;
+            }
+        

3652be9490176c7d626b903376a6e5ef29d060db
beurre369 <58581514+beurre369@users.noreply.github.com>
2020-02-07 14:03:01 +0900

    Create fetchMovie.js

---
+++ fetchMovie.js
@@ -0,0 +1,29 @@
+function fetcMovie(name){
+        fetch(name).then(function(response){
+          response.text().then(function(text){
+            document.querySelector('article.column.middle').innerHTML = text;
+             })
+          });
+        }
+      if(location.hash){
+       fetchMovie(location.hash.substr(2));
+      }else{
+        fetchMovie('welcome');
+      }
+      fetch('movieList').then(function(response){
+          response.text().then(function(text){
+            console.log(text);
+            var items = text.split(',');
+            var i =0;
+            var tags = '';
+            while(i<items.length){
+              var item = items[i];
+              item = item.trim();
+              var tag = '<li><a href="#!'+item+'" onclick="fetchMovie(\''+item+'\')">'+item+'</a></li>';
+              console.log(item);
+              tags = tags + tag;
+              i = i+1;
+            }
+            document.querySelector('#reviewnav').innerHTML = tags;
+             })
+          });
